ce8923c8524d8e5bc9ca741f570655f28d0ba9ee
seb <seb.chevre>
2017-02-14 16:18:38 +0100

    initial commit

--- src/main/java/ch/sebooom/pocjms/infrastructure/jms/JmsPublisher.java
+++ src/main/java/ch/sebooom/pocjms/infrastructure/jms/JmsPublisher.java
@@ -15,15 +15,17 @@ public class JmsPublisher<T> implements Publisher<T>,ExceptionListener{
     private static final Logger logger = Logger.getLogger(JmsPublisher.class.getName());
     private Connection connection = null;
 
-    public JmsPublisher (ConnectionFactory c) {
+    public static JmsPublisher get(ConnectionFactory c) {
 
         try {
-            connection = c.createConnection();
-            connection.start();
-            connection.setExceptionListener(this);
+
+            JmsPublisher p = new JmsPublisher();
+            p.connection = c.createConnection();
+            p.connection.start();
+            p.connection.setExceptionListener(p);
 
             // session
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Session session = p.connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
             // destination
             Destination destination = session.createQueue("TEST.FOO-1");
@@ -32,27 +34,28 @@ public class JmsPublisher<T> implements Publisher<T>,ExceptionListener{
             MessageProducer producer = session.createProducer(destination);
             producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
 
-            // Create a messages
-            String text = "Hello world! From: " + Thread.currentThread().getName() + " : " + this.hashCode();
-            TextMessage message = session.createTextMessage(text);
 
-            // Tell the producer to send the message
-            logger.info("Sent message: "+ message.hashCode() + " : " + Thread.currentThread().getName());
-            producer.send(message);
-
-            // Clean up
-            session.close();
-            connection.close();
 
 
         } catch (JMSException e) {
             logger.info(e.getMessage());
         }
-
+        return null;
     }
 
     public void publish(T message) {
 
+        // Create a messages
+        String text = "Hello world! From: " + Thread.currentThread().getName() + " : " + this.hashCode();
+      //  TextMessage message = session.createTextMessage(text);
+
+        // Tell the producer to send the message
+        logger.info("Sent message: "+ message.hashCode() + " : " + Thread.currentThread().getName());
+        //producer.send(message);
+
+        // Clean up
+        //session.close();
+        //connection.close();
     }
 
     public void onException(JMSException e) {
