f20a934f12453587634329410e65154c30e0b4c4
sebChevre <seb.chevre@gmail.com>
2017-03-07 15:46:58 +0100

    commit

---
+++ jwtsample/src/main/java/ch/sebooom/model/security/AuthorityName.java
@@ -0,0 +1,5 @@
+package ch.sebooom.model.security;
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ jwtsample/src/main/java/ch/sebooom/security/service/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package ch.sebooom.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ch.sebooom.model.security.User;
+import ch.sebooom.security.JwtUserFactory;
+import ch.sebooom.security.repository.UserRepository;
+
+/**
+ * Created by stephan on 20.03.16.
+ */
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("No user found with username '%s'.", username));
+        } else {
+            return JwtUserFactory.create(user);
+        }
+    }
+}
